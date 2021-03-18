ca991868e9fcd13cf05806ec50bac72020791fef
BenAAndrew <bandrew01@qub.ac.uk>
2021-03-18 18:08:28 +0000

    Create compress.py

---
+++ dataset/compress.py
@@ -0,0 +1,18 @@
+from subprocess import call
+
+TARGET_SAMPLE_RATE = 22050
+TARGET_BITRATE = "32k"
+
+def compress_audio(input_path, output_path):
+    command = (
+        f"ffmpeg -i {input_path} -acodec libmp3lame -b:a {TARGET_BITRATE} -ac 1 -ar {TARGET_SAMPLE_RATE} {output_path}"
+    )
+    call(command.split(" "))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_path", type=str, help="input audio path")
+    parser.add_argument("-o", "--output_path", type=str, help="output audio path")
+    args = parser.parse_args()
+
+    compress_audio(args.input_path, args.output_path)
