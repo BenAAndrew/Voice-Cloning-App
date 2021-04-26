28166ad65febf031650c82441b73476f8acc2b6a
BenAAndrew <bandrew01@qub.ac.uk>
2021-04-26 11:39:26 +0100

    Remove init method to use default filesystem

--- training/train.py
+++ training/train.py
@@ -95,7 +95,6 @@ def train(
         # Initialize distributed communication
         dist.init_process_group(
             backend="nccl", 
-            init_method="tcp://localhost:54321",
             world_size=num_gpus, 
             rank=0
         )
