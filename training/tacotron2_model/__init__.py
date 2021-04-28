3b1895f322498963ef66d57f31c1b8bedb7b52af
BenAAndrew <bandrew01@qub.ac.uk>
2021-04-28 14:25:01 +0100

    Remove usages of pypi package

--- training/tacotron2_model/__init__.py
+++
@@ -1,4 +0,0 @@
-from tacotron2_model.model import Tacotron2
-from tacotron2_model.loss import Tacotron2Loss
-from tacotron2_model.collate import TextMelCollate
-from tacotron2_model.stft import TacotronSTFT
