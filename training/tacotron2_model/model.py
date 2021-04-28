66194cce6a53ee95f2d7e61b4208e0b5ae4e4c48
BenAAndrew <bandrew01@qub.ac.uk>
2021-04-28 17:20:25 +0100

    Fix size print

--- training/tacotron2_model/model.py
+++ training/tacotron2_model/model.py
@@ -568,7 +568,7 @@ class Tacotron2(nn.Module):
             outputs[1].data.masked_fill_(mask, 0.0)
             outputs[2].data.masked_fill_(mask[:, 0, :], 1e3)  # gate energies
 
-        print("OUTPUT SIZE", outputs.size())
+        print("OUTPUT SIZE", outputs[0].size(), outputs[1].size(), outputs[2].size())
 
         return outputs
 
