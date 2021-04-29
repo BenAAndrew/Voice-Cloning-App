9866280937423dd94f25c028f1756cb4625a3fd9
BenAAndrew <bandrew01@qub.ac.uk>
2021-04-29 17:33:59 +0100

    Update validate model forward

--- training/validate.py
+++ training/validate.py
@@ -28,8 +28,8 @@ def validate(model, val_loader, criterion, iteration):
     with torch.no_grad():
         val_loss = 0.0
         for i, batch in enumerate(val_loader):
-            x, y = parse_batch(batch)
-            y_pred = model(x)
+            x, y, mask_size, alignment_mask_size = parse_batch(batch)
+            y_pred = model(x, mask_size=mask_size, alignment_mask_size=alignment_mask_size)
             loss = criterion(y_pred, y)
             reduced_val_loss = loss.item()
             val_loss += reduced_val_loss
