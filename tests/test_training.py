5d185acdc4e005eec309986ec4508816707db8e6
BenAAndrew <bandrew01@qub.ac.uk>
2021-07-26 17:42:14 +0100

    Improve training test coverage

--- tests/test_training.py
+++ tests/test_training.py
@@ -3,11 +3,15 @@ import inflect
 import random
 from string import ascii_lowercase
 from unittest import mock
+import torch
+import shutil
 
 from dataset.clip_generator import CHARACTER_ENCODING
 from training.clean_text import clean_text
+from training.checkpoint import load_checkpoint, save_checkpoint, warm_start_model
 from training.dataset import VoiceDataset
-from training.train import DEFAULT_ALPHABET
+from training.tacotron2_model import Tacotron2
+from training.train import DEFAULT_ALPHABET, WEIGHT_DECAY
 from training.utils import check_space, load_symbols, CHECKPOINT_SIZE_MB, PUNCTUATION
 
 
@@ -31,6 +35,48 @@ def test_voice_dataset():
     text, mel = dataset[0]
     assert len(text) == len("that five shots may have been")
     assert mel.shape[0] == 80
+
+
+# Checkpoints
+def test_load_and_save_checkpoint():
+    model_path = os.path.join("test_samples", "model.pt")
+    model = Tacotron2()
+    lr = 0.1
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=WEIGHT_DECAY)
+    model, optimizer, iteration, epoch = load_checkpoint(model_path, model, optimizer, [None] * 10000)
+
+    assert iteration == 510000
+    assert epoch == iteration // 10000
+
+    checkpoint_folder = "test-checkpoints"
+    os.makedirs(checkpoint_folder)
+    save_checkpoint(model, optimizer, lr, iteration, epoch, checkpoint_folder)
+    assert "checkpoint_510000" in os.listdir(checkpoint_folder)
+
+    # Overwrite existing
+    save_checkpoint(model, optimizer, lr, 520000, epoch, checkpoint_folder, overwrite_checkpoints=True)
+    assert "checkpoint_520000" in os.listdir(checkpoint_folder)
+    assert "checkpoint_510000" not in os.listdir(checkpoint_folder)
+
+    # Do not overwrite existing
+    save_checkpoint(model, optimizer, lr, 530000, epoch, checkpoint_folder, overwrite_checkpoints=False)
+    assert "checkpoint_520000" in os.listdir(checkpoint_folder)
+    assert "checkpoint_530000" in os.listdir(checkpoint_folder)
+
+    shutil.rmtree(checkpoint_folder)
+
+
+def test_warm_start_model():
+    model_path = os.path.join("test_samples", "model.pt")
+    model = Tacotron2()
+    ignore_layers=["embedding.weight"]
+    model = warm_start_model(model_path, model, ignore_layers)
+    model_dict = model.state_dict()
+
+    checkpoint_dict = torch.load(model_path, map_location="cpu")["state_dict"]
+    for k in checkpoint_dict.keys():
+        if k not in ignore_layers:
+            assert torch.equal(model_dict[k], checkpoint_dict[k])
 
 
 # Symbols
