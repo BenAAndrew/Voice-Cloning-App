3b5a51ba8f3d300f97213e68569a7832994b315d
BenAAndrew <bandrew01@qub.ac.uk>
2021-08-01 18:08:47 +0100

    improve test_hifigan_synthesis

--- tests/test_synthesis.py
+++ tests/test_synthesis.py
@@ -1,14 +1,12 @@
 import os
-import inflect
 import numpy as np
 import librosa
 from unittest import mock
-import pytest
 
 from dataset.forced_alignment.search import similarity
 from dataset.transcribe import create_transcription_model
 from synthesis.synthesize import load_model, synthesize
-from synthesis.vocoders import Waveglow, Hifigan
+from synthesis.vocoders import Hifigan
 from synthesis.vocoders.vocoder import Vocoder
 
 
@@ -75,60 +73,56 @@ def test_synthesize(clean_text):
     os.remove(audio_path)
 
 
-@pytest.mark.slow
-def test_waveglow_synthesis():
-    model_path = os.path.join("test_samples", "model.pt")
-    waveglow_path = os.path.join("test_samples", "waveglow_256channels_universal_v5.pt")
-    graph_path = "graph.png"
-    audio_path = "synthesized_audio.wav"
-    transcription_model = create_transcription_model()
+# @pytest.mark.slow
+# def test_waveglow_synthesis():
+#     model_path = os.path.join("test_samples", "model.pt")
+#     waveglow_path = os.path.join("test_samples", "waveglow_256channels_universal_v5.pt")
+#     graph_path = "graph.png"
+#     audio_path = "synthesized_audio.wav"
+#     transcription_model = create_transcription_model()
 
-    model = load_model(model_path)
-    waveglow = Waveglow(waveglow_path)
-    text = "hello everybody my name is david attenborough"
-    inflect_engine = inflect.engine()
-    synthesize(
-        model=model,
-        text=text,
-        inflect_engine=inflect_engine,
-        graph_path=graph_path,
-        audio_path=audio_path,
-        vocoder=waveglow,
-    )
+#     model = load_model(model_path)
+#     waveglow = Waveglow(waveglow_path)
+#     text = "hello everybody my name is david attenborough"
+#     inflect_engine = inflect.engine()
+#     synthesize(
+#         model=model,
+#         text=text,
+#         inflect_engine=inflect_engine,
+#         graph_path=graph_path,
+#         audio_path=audio_path,
+#         vocoder=waveglow,
+#     )
 
-    assert os.path.isfile(graph_path)
-    assert os.path.isfile(audio_path)
-    assert similarity(text, transcription_model.transcribe(audio_path)) > MIN_SYNTHESIS_SCORE
+#     assert os.path.isfile(graph_path)
+#     assert os.path.isfile(audio_path)
+#     assert similarity(text, transcription_model.transcribe(audio_path)) > MIN_SYNTHESIS_SCORE
 
-    os.remove(graph_path)
-    os.remove(audio_path)
+#     os.remove(graph_path)
+#     os.remove(audio_path)
 
 
-@pytest.mark.slow
-def test_hifigan_synthesis():
+@mock.patch("synthesis.synthesize.clean_text", side_effect=lambda text, engine: text)
+def test_hifigan_synthesis(clean_text):
     model_path = os.path.join("test_samples", "model.pt")
     hifigan_model_path = os.path.join("test_samples", "hifigan.pt")
     hifigan_config_path = os.path.join("test_samples", "config.json")
-    graph_path = "graph.png"
     audio_path = "synthesized_audio.wav"
     transcription_model = create_transcription_model()
 
     model = load_model(model_path)
     hifigan = Hifigan(hifigan_model_path, hifigan_config_path)
     text = "hello everybody my name is david attenborough"
-    inflect_engine = inflect.engine()
     synthesize(
         model=model,
         text=text,
-        inflect_engine=inflect_engine,
-        graph_path=graph_path,
+        inflect_engine=None,
+        graph_path=None,
         audio_path=audio_path,
         vocoder=hifigan,
     )
 
-    assert os.path.isfile(graph_path)
     assert os.path.isfile(audio_path)
     assert similarity(text, transcription_model.transcribe(audio_path)) > MIN_SYNTHESIS_SCORE
 
-    os.remove(graph_path)
     os.remove(audio_path)
