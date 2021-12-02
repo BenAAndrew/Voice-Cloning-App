f67d1ef9814bf071dec42ef26a73e0b13c58ab72
BenAAndrew <bandrew01@qub.ac.uk>
2021-12-02 20:12:30 +0000

    init

---
+++ synthesis/hifigan_training/meldataset.py
@@ -0,0 +1,135 @@
+import math
+import os
+import random
+import torch
+import torch.utils.data
+import numpy as np
+from librosa.util import normalize
+from scipy.io.wavfile import read
+from librosa.filters import mel as librosa_mel_fn
+
+MAX_WAV_VALUE = 32768.0
+
+
+def load_wav(full_path):
+    sampling_rate, data = read(full_path)
+    return data, sampling_rate
+
+
+def dynamic_range_compression(x, C=1, clip_val=1e-5):
+    return np.log(np.clip(x, a_min=clip_val, a_max=None) * C)
+
+
+def dynamic_range_decompression(x, C=1):
+    return np.exp(x) / C
+
+
+def dynamic_range_compression_torch(x, C=1, clip_val=1e-5):
+    return torch.log(torch.clamp(x, min=clip_val) * C)
+
+
+def dynamic_range_decompression_torch(x, C=1):
+    return torch.exp(x) / C
+
+
+def spectral_normalize_torch(magnitudes):
+    output = dynamic_range_compression_torch(magnitudes)
+    return output
+
+
+def spectral_de_normalize_torch(magnitudes):
+    output = dynamic_range_decompression_torch(magnitudes)
+    return output
+
+
+mel_basis = {}
+hann_window = {}
+
+
+def mel_spectrogram(y, n_fft, num_mels, sampling_rate, hop_size, win_size, fmin, fmax, center=False):
+    if torch.min(y) < -1.:
+        print('min value is ', torch.min(y))
+    if torch.max(y) > 1.:
+        print('max value is ', torch.max(y))
+
+    global mel_basis, hann_window
+    if fmax not in mel_basis:
+        mel = librosa_mel_fn(sampling_rate, n_fft, num_mels, fmin, fmax)
+        mel_basis[str(fmax)+'_'+str(y.device)] = torch.from_numpy(mel).float().to(y.device)
+        hann_window[str(y.device)] = torch.hann_window(win_size).to(y.device)
+
+    y = torch.nn.functional.pad(y.unsqueeze(1), (int((n_fft-hop_size)/2), int((n_fft-hop_size)/2)), mode='reflect')
+    y = y.squeeze(1)
+
+    spec = torch.stft(y, n_fft, hop_length=hop_size, win_length=win_size, window=hann_window[str(y.device)],
+                      center=center, pad_mode='reflect', normalized=False, onesided=True)
+
+    spec = torch.sqrt(spec.pow(2).sum(-1)+(1e-9))
+
+    spec = torch.matmul(mel_basis[str(fmax)+'_'+str(y.device)], spec)
+    spec = spectral_normalize_torch(spec)
+
+    return spec
+
+
+class MelDataset(torch.utils.data.Dataset):
+    def __init__(self, training_files, segment_size, n_fft, num_mels,
+                 hop_size, win_size, sampling_rate,  fmin, fmax, split=True, shuffle=True, n_cache_reuse=1,
+                 device=None, fmax_loss=None):
+        self.audio_files = training_files
+        random.seed(1234)
+        if shuffle:
+            random.shuffle(self.audio_files)
+        self.segment_size = segment_size
+        self.sampling_rate = sampling_rate
+        self.split = split
+        self.n_fft = n_fft
+        self.num_mels = num_mels
+        self.hop_size = hop_size
+        self.win_size = win_size
+        self.fmin = fmin
+        self.fmax = fmax
+        self.fmax_loss = fmax_loss
+        self.cached_wav = None
+        self.n_cache_reuse = n_cache_reuse
+        self._cache_ref_count = 0
+        self.device = device
+
+    def __getitem__(self, index):
+        filename = self.audio_files[index]
+        if self._cache_ref_count == 0:
+            audio, sampling_rate = load_wav(filename)
+            audio = audio / MAX_WAV_VALUE
+            audio = normalize(audio) * 0.95
+            self.cached_wav = audio
+            if sampling_rate != self.sampling_rate:
+                raise ValueError("{} SR doesn't match target {} SR".format(
+                    sampling_rate, self.sampling_rate))
+            self._cache_ref_count = self.n_cache_reuse
+        else:
+            audio = self.cached_wav
+            self._cache_ref_count -= 1
+
+        audio = torch.FloatTensor(audio)
+        audio = audio.unsqueeze(0)
+
+        if self.split:
+            if audio.size(1) >= self.segment_size:
+                max_audio_start = audio.size(1) - self.segment_size
+                audio_start = random.randint(0, max_audio_start)
+                audio = audio[:, audio_start:audio_start+self.segment_size]
+            else:
+                audio = torch.nn.functional.pad(audio, (0, self.segment_size - audio.size(1)), 'constant')
+
+        mel = mel_spectrogram(audio, self.n_fft, self.num_mels,
+                                self.sampling_rate, self.hop_size, self.win_size, self.fmin, self.fmax,
+                                center=False)
+
+        mel_loss = mel_spectrogram(audio, self.n_fft, self.num_mels,
+                                   self.sampling_rate, self.hop_size, self.win_size, self.fmin, self.fmax_loss,
+                                   center=False)
+
+        return (mel.squeeze(), audio.squeeze(0), filename, mel_loss.squeeze())
+
+    def __len__(self):
+        return len(self.audio_files)
---
+++ synthesis/hifigan_training/train_hifigan.py
@@ -0,0 +1,158 @@
+import torch
+from torch.utils.data import DistributedSampler, DataLoader
+import torch.nn.functional as F
+import json
+import itertools
+import os
+
+from meldataset import MelDataset, mel_spectrogram
+from models import Generator, MultiPeriodDiscriminator, MultiScaleDiscriminator, feature_loss, generator_loss, discriminator_loss
+from utils import AttrDict, save_checkpoint
+
+SEED = 1234
+CONFIG_FILE = "config.json"
+TRAIN_SIZE = 0.66
+
+def train_hifigan(audio_folder, output_directory, epochs=1, batch_size=1, iters_per_checkpoint=10):
+    assert torch.cuda.is_available(), "You do not have Torch with CUDA installed. Please check CUDA & Pytorch install"
+    os.makedirs(output_directory, exist_ok=True)
+    
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+    
+    device = torch.device("cuda")
+    audio = [os.path.join(audio_folder, name) for name in os.listdir(audio_folder)]
+
+    with open(CONFIG_FILE) as f:
+        params = AttrDict(json.loads(f.read()))
+        generator = Generator(params).to(device)
+
+    mpd = MultiPeriodDiscriminator().to(device)
+    msd = MultiScaleDiscriminator().to(device)
+
+    optim_g = torch.optim.AdamW(generator.parameters(), params.learning_rate, betas=[params.adam_b1, params.adam_b2])
+    optim_d = torch.optim.AdamW(itertools.chain(msd.parameters(), mpd.parameters()), params.learning_rate, betas=[params.adam_b1, params.adam_b2])
+
+    state_dict_do = None
+    last_epoch = -1
+
+    train_cutoff = int(len(audio) * TRAIN_SIZE)
+    train_files = audio[:train_cutoff]
+    test_files = audio[train_cutoff:]
+    print(train_files, test_files)
+
+    scheduler_g = torch.optim.lr_scheduler.ExponentialLR(optim_g, gamma=params.lr_decay, last_epoch=last_epoch)
+    scheduler_d = torch.optim.lr_scheduler.ExponentialLR(optim_d, gamma=params.lr_decay, last_epoch=last_epoch)
+
+    trainset = MelDataset(train_files, params.segment_size, params.n_fft, params.num_mels,
+                          params.hop_size, params.win_size, params.sampling_rate, params.fmin, params.fmax, n_cache_reuse=0,
+                          shuffle=True, fmax_loss=params.fmax_for_loss, device=device)
+
+    train_sampler = DistributedSampler(trainset) if params.num_gpus > 1 else None
+    train_loader = DataLoader(trainset, num_workers=params.num_workers, shuffle=False,
+                              sampler=train_sampler,
+                              batch_size=batch_size,
+                              pin_memory=True,
+                              drop_last=True)
+
+    validset = MelDataset(test_files, params.segment_size, params.n_fft, params.num_mels,
+                              params.hop_size, params.win_size, params.sampling_rate, params.fmin, params.fmax, False, False, n_cache_reuse=0,
+                              fmax_loss=params.fmax_for_loss, device=device)
+    validation_loader = DataLoader(validset, num_workers=1, shuffle=False,
+                                    sampler=None,
+                                    batch_size=batch_size,
+                                    pin_memory=True,
+                                    drop_last=True)
+
+    generator.train()
+    mpd.train()
+    msd.train()
+    iterations = 0
+    for epoch in range(max(0, last_epoch), epochs):
+        print("Epoch: {}".format(epoch+1))
+
+        for _, batch in enumerate(train_loader):
+            print(len(batch))
+            x, y, _, y_mel = batch
+            x = torch.autograd.Variable(x.to(device, non_blocking=True))
+            y = torch.autograd.Variable(y.to(device, non_blocking=True))
+            y_mel = torch.autograd.Variable(y_mel.to(device, non_blocking=True))
+            y = y.unsqueeze(1)
+
+            y_g_hat = generator(x)
+            y_g_hat_mel = mel_spectrogram(y_g_hat.squeeze(1), params.n_fft, params.num_mels, params.sampling_rate, params.hop_size, params.win_size,
+                                          params.fmin, params.fmax_for_loss)
+
+            optim_d.zero_grad()
+
+            # MPD
+            y_df_hat_r, y_df_hat_g, _, _ = mpd(y, y_g_hat.detach())
+            loss_disc_f, losses_disc_f_r, losses_disc_f_g = discriminator_loss(y_df_hat_r, y_df_hat_g)
+
+            # MSD
+            y_ds_hat_r, y_ds_hat_g, _, _ = msd(y, y_g_hat.detach())
+            loss_disc_s, losses_disc_s_r, losses_disc_s_g = discriminator_loss(y_ds_hat_r, y_ds_hat_g)
+
+            loss_disc_all = loss_disc_s + loss_disc_f
+
+            loss_disc_all.backward()
+            optim_d.step()
+
+            # Generator
+            optim_g.zero_grad()
+
+            # L1 Mel-Spectrogram Loss
+            loss_mel = F.l1_loss(y_mel, y_g_hat_mel) * 45
+
+            y_df_hat_r, y_df_hat_g, fmap_f_r, fmap_f_g = mpd(y, y_g_hat)
+            y_ds_hat_r, y_ds_hat_g, fmap_s_r, fmap_s_g = msd(y, y_g_hat)
+            loss_fm_f = feature_loss(fmap_f_r, fmap_f_g)
+            loss_fm_s = feature_loss(fmap_s_r, fmap_s_g)
+            loss_gen_f, losses_gen_f = generator_loss(y_df_hat_g)
+            loss_gen_s, losses_gen_s = generator_loss(y_ds_hat_g)
+            loss_gen_all = loss_gen_s + loss_gen_f + loss_fm_s + loss_fm_f + loss_mel
+
+            loss_gen_all.backward()
+            optim_g.step()
+
+            with torch.no_grad():
+                mel_error = F.l1_loss(y_mel, y_g_hat_mel).item()
+
+            print('Steps : {:d}, Gen Loss Total : {:4.3f}, Mel-Spec. Error : {:4.3f}'.format(iterations, loss_gen_all, mel_error))
+
+            if iterations % iters_per_checkpoint == 0:
+                # save checkpoint
+                checkpoint_path = "{}/g_{:08d}".format(output_directory, iterations)
+                save_checkpoint(checkpoint_path, {'generator': (generator).state_dict()})
+                checkpoint_path = "{}/do_{:08d}".format(output_directory, iterations)
+                save_checkpoint(checkpoint_path, 
+                                {'mpd': mpd.state_dict(), 'msd': msd.state_dict(), 
+                                'optim_g': optim_g.state_dict(), 'optim_d': optim_d.state_dict(), 
+                                'steps': iterations, 'epoch': epoch})
+
+                # validate
+                generator.eval()
+                torch.cuda.empty_cache()
+                val_err_tot = 0
+                with torch.no_grad():
+                    for j, batch in enumerate(validation_loader):
+                        x, y, _, y_mel = batch
+                        y_g_hat = generator(x.to(device))
+                        y_mel = torch.autograd.Variable(y_mel.to(device, non_blocking=True))
+                        y_g_hat_mel = mel_spectrogram(y_g_hat.squeeze(1), params.n_fft, params.num_mels, params.sampling_rate,
+                                                        params.hop_size, params.win_size, params.fmin, params.fmax_for_loss)
+                        val_err_tot += F.l1_loss(y_mel, y_g_hat_mel).item()
+
+                    val_err = val_err_tot / (j+1)
+                    print("validation error", val_err)
+
+                generator.train()
+
+            iterations += 1
+
+        scheduler_g.step()
+        scheduler_d.step()
+
+
+if __name__ == "__main__":
+    train_hifigan("wavs", "checkpoints")
