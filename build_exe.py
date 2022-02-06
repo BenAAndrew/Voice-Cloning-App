1616dd31886028bc0e4e7b70051d576d8cefe72e
Ben Andrew <benandrew89@gmail.com>
2022-02-06 15:50:16 +0000

    Update build_exe.py

--- build_exe.py
+++ build_exe.py
@@ -7,28 +7,21 @@ from PyInstaller.utils.hooks import get_package_paths
 if __name__ == "__main__":
     assert os.path.isfile("ffmpeg\\bin\\ffmpeg.exe")
     assert os.path.isfile("en_v5.jit")
+    assert os.path.isfile("application\\static\\favicon\\app-icon.ico")
     pytorch_libs = os.path.join(get_package_paths("torch")[1], "lib")
     PyInstaller.__main__.run(
         [
             "main.py",
             "--onefile",
             "--clean",
-            "--icon=application\static\\favicon\\app-icon.ico",
+            "--icon=application\\static\\favicon\\app-icon.ico",
+            "--additional-hooks=extra-hooks",
+            "--hidden-import=scipy.special.cython_special",
             "--add-data=application/static;application/static",
             "--add-data=alphabets;alphabets",
             "--add-data=training/hifigan;training/hifigan",
-            "--additional-hooks=extra-hooks",
-            "--hidden-import=sklearn.utils._weight_vector",
-            "--hidden-import=sklearn.utils._cython_blas",
-            "--hidden-import=sklearn.neighbors._typedefs",
-            "--hidden-import=sklearn.neighbors._quad_tree",
-            "--hidden-import=sklearn.tree._utils",
-            "--hidden-import=sklearn.tree",
-            "--hidden-import=scipy.special.cython_special",
             "--add-data=ffmpeg/bin/ffmpeg.exe;.",
             "--add-data=en_v5.jit;.",
-            "--add-data",
-            pytorch_libs + ";.",
         ]
     )
     shutil.rmtree("build")
